d20065fbd9de1d8207f13fcdec16bfd671cf2f5d
banupriya-by <bybanupriya@gmail.com>
2026-02-20 23:03:27 +0530

    Program Done

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Main {
 
     public int fibonacci(int n) {
+        int k;
         if (n == 0)
             return 0;
         if (n == 1)
             return 1;
         else
-            return fibonacci(n - 1) + fibonacci(n - 2);
+            return k = fibonacci(n - 1) + fibonacci(n - 2);
     }
 
     public static void main(String[] args) {
@@ -17,10 +18,22 @@ public class Main {
 
         int n = scanner.nextInt();
 
+//        To print fibonacci series
+        System.out.print(0 + "\t");
+        System.out.print(1 + "\t");
+        int a = 0;
+        int b = 1;
+        for (int i = 1; i < n; i++) {
+            int c = a + b;
+            System.out.print(c + "\t");
+            a = b;
+            b = c;
+        }
+
         Main m = new Main();
 
 //      printing the place of the number in the fibonacci series
-        System.out.println(m.fibonacci(n));
+        System.out.println("\n" + m.fibonacci(n));
 
 
     }
