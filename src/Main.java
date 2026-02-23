f06054195867549b7e2895265e4702de17f50d87
banupriya-by <bybanupriya@gmail.com>
2026-02-23 23:10:23 +0530

    Program Done

--- src/Main.java
+++ src/Main.java
@@ -23,18 +23,22 @@ public class Main {
         System.out.print(1 + "\t");
         int a = 0;
         int b = 1;
+        int sum = a + b;
+
         for (int i = 1; i < n; i++) {
             int c = a + b;
             System.out.print(c + "\t");
             a = b;
             b = c;
+            sum = c + sum; // sum of fibonacci series
         }
+
 
         Main m = new Main();
 
 //      printing the place of the number in the fibonacci series
         System.out.println("\n" + m.fibonacci(n));
 
-
+        System.out.println("Sum of the fibonacci series : " + sum);
     }
 }
